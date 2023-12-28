30d1b65f99d85a88add6a30d060fe12cf65ed69d
TheWhiteMachine <priveiro@gmail.com>
2023-12-28 15:33:40 -0300

    inprouving UI and no empty task is added, adding footer

--- src/App.js
+++ src/App.js
@@ -6,43 +6,71 @@ import TodoForm from './components/TodoForm'
 import Presentacion from './components/Presentacion';
 
 function App() {
-  const [todos, setTodos] = useState(data);
+	const [todos, setTodos] = useState(data);
 
-  const onComplete = (id) => {
-    console.log('task', id);
+	const onComplete = (id) => {
+		console.log('task', id);
 
-    setTodos(todos.map((todo) => {
-      return todo.id === Number(id) ? { ...todo, complete: !todo.complete } : { ...todo }
-    }))
-  }
+		setTodos(todos.map((todo) => {
+			return todo.id === Number(id) ? { ...todo, complete: !todo.complete } : { ...todo }
+		}))
+	}
 
-  const onDeleteItem = (id) => {
-    setTodos([...todos].filter(todo => todo.id !== id))
-  }
+	const onDeleteItem = (id) => {
+		setTodos([...todos].filter(todo => todo.id !== id))
+	}
 
-  const addTodo = (newTodo) => {
-    console.log('agregado', newTodo)
-    let newItem = { id: +new Date(), task: newTodo, complete: false };
-    setTodos([...todos, newItem]);
+	const addTodo = (newTodo) => {
+		console.log('agregado', newTodo)
+		let newItem = { id: +new Date(), task: newTodo, complete: false };
+		setTodos([...todos, newItem]);
 
-  }
+	}
 
-  return (
-    <>
-      <div className="container">
-        <div className="titulo">
-          <h1>Lista de Tareas</h1>
-        </div>
-        <div className="inputNew">
-          <TodoForm addTodo={addTodo} />
-        </div>
+	return (
+		<>
+			<div className='principal'>
 
-        <TodoList todos={todos} onComplete={onComplete} onDeleteItem={onDeleteItem} />
-      </div>
-      <Presentacion />
-    </>
+				<Presentacion />
+				<div className="container">
+					<div className="titulo">
+						<h1>Lista de Tareas</h1>
+					</div>
+					<div className="inputNew">
+						<TodoForm addTodo={addTodo} />
+					</div>
 
-  );
+					<TodoList todos={todos} onComplete={onComplete} onDeleteItem={onDeleteItem} />
+				</div>
+				<div className="footer">
+					<div className="item">
+						<p>	<span className="material-symbols-outlined">
+							home
+						</span>
+						</p>
+
+						<p>Take a look on my <a href='https://www.linkedin.com/in/pablo-riveiro-uy/'>LinkedIn</a></p>
+					</div>
+					<div className="item">
+						<p>
+
+							<span class="material-symbols-outlined">
+								terminal
+							</span>
+						</p>
+
+
+						<p>
+							Visit my <a href='https://github.com/pablo-riveiro-uy'> Github</a>
+						</p>
+					</div>
+
+
+				</div>
+			</div >
+		</>
+
+	);
 }
 
 export default App;
--- src/components/Presentacion.js
+++ src/components/Presentacion.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 export default function Presentacion() {
-    return (
-        <div className='presenta'>
-            <img src='https://scontent.fmvd1-1.fna.fbcdn.net/v/t39.30808-6/278072707_10227076612889422_2267551032763661716_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=yKUBm0N_e1UAX-i8eXR&_nc_ht=scontent.fmvd1-1.fna&oh=00_AT_bPc8n6f47p8cCYHSs9Bp1roLByEgzmTS5ionxC6_Xrg&oe=630E778F' />
-            <h2>HOLA</h2>
-            <p>
-                Mi nombre Es <span>Pablo Riveiro</span>, estoy preparándome para ser desarrollador y estoy dando mis primeros pasos.
-            </p>
+	return (
+		<div className='presenta'>
 
-        </div>
-    )
+
+			<img src="https://i.ibb.co/SdGgGZz/foto-perfil.jpg" alt="profile-pic" border="0" />
+
+			<h2>HOLA</h2>
+			<p>
+				Mi nombre es <br /> <span>Pablo Riveiro</span>, esta fue una de las primeras apps que construí en React
+			</p>
+
+		</div >
+	)
 }
